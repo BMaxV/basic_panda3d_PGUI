e1c269adb5d0b09f2a4cbd17769a91a88145dffc
Max <bruno.m.voss@gmail.com>
2024-02-01 21:27:45 +0100

    more PGUI

--- main.py
+++ main.py
@@ -1,46 +1,101 @@
 from panda3d.core import PGButton
 from panda3d.core import PGVirtualFrame
 from panda3d.core import NodePath
+from panda3d.core import PGEntry
+from panda3d.core import PGScrollFrame
+
+# this cointains all the awkwardly setup hardcoded signals.
+from direct.gui.DirectGui import DGG
+print(DGG.B1CLICK)
+
+from direct.showbase import DirectObject
 
 from direct.showbase.ShowBase import ShowBase
 
+#draw_mask
+# what's that? found in pgItem.cxx
+
 class Wrapper:
-    def __init__(self):
-        self.b = ShowBase()
+	def __init__(self):
+		self.b = ShowBase()
+		self.event_handler = DirectObject.DirectObject()
+			
+		b = PGButton("hello") # this is not the text, this is the NAME
+		b2 = PGButton("there")
+		F = PGVirtualFrame("Marco")
+		F2 = PGVirtualFrame("Polo")
+		
+		E = PGEntry("Lima")
+		SF = PGScrollFrame("Alice")
+		
+		b.setup("hello text",0.1) # display text and the bevel width
+		b2.setup("hunter2",0.5) 
+		
+		F.setup(0.2,0.3) # dimensions
+		F2.setup(0.5,0.1)
+		
+		E.setup(10,3) # width and lines
+		
+		# big frame, (inner x4), slider width, bevel
+		SF.setup(0.5,0.5,-0.4,0.4,-0.8,0.8,0.1,0.05)
+			
+		button_node_path = self.b.aspect2d.attachNewNode(b)
+		button_node_path2 = self.b.aspect2d.attachNewNode(b2)
+		
+		frame_node_path = self.b.aspect2d.attachNewNode(F)
+		frame_node_path2 = self.b.aspect2d.attachNewNode(F2)
+		
+		entry_path = self.b.aspect2d.attachNewNode(E)
+		SF_path = self.b.aspect2d.attachNewNode(SF)
+		
+		my_id = b.get_id()
+		print(my_id)
+		
+		# this is how to combine the hardcoded signals (see DGG above)
+		# with the id of the button
+		self.event_handler.accept("within-"+str(my_id),print,["hello there"])
+		self.event_handler.accept("without-"+str(my_id),print,["goodbye"])
+		self.event_handler.accept("click-mouse1-"+str(my_id),print,["ACTIVATE"])
+		
+		# which didn't work for the frames for some reason
+		my_id = F.get_id()
+		print(my_id)
+		self.event_handler.accept("within-"+str(my_id),print,["hello there"])
+		
+		my_id = F2.get_id()
+		print(my_id)
+		self.event_handler.accept("within-"+str(my_id),print,["hello there"])
+		
+		
+		# this is some positioning but it basically works just
+		# as you would expect, so it's not really worth mentioning.
+		
+		button_node_path.set_scale(0.15)
+		button_node_path.set_pos(-0.8,0,0.15)
+		
+		button_node_path2.set_scale(0.3)
+		button_node_path2.set_pos(0,0,0.5)
+		
+		frame_node_path.set_scale(1)
+		frame_node_path.set_pos(-0.3,0,-0.3)
+		frame_node_path.set_hpr(0,0,45)
+		
+		frame_node_path2.set_scale(1)
+		frame_node_path2.set_pos(0.3,0,-0.5)
+		frame_node_path2.set_hpr(45,45,0)
+		
+		entry_path.set_scale(0.1)
+		entry_path.set_pos(0,0,0.15)
+		
+		SF_path.set_scale(1)
+		SF_path.set_pos(0.8,0,-0.8)
 
 def main():
-    W = Wrapper()
-    b = PGButton("hello") # this is not the text, this is the NAME
-    b2 = PGButton("there")
-    F = PGVirtualFrame("Marco")
-    F2 = PGVirtualFrame("Polo")
-    
-    b.setup("hello text",0.1) #this sets up the text on the button and the bevel width
-    b2.setup("hunter2",0.5) 
-    F.setup(0.2,0.3)
-    F2.setup(0.5,0.1)
-        
-    button_node_path = W.b.aspect2d.attachNewNode(b)
-    button_node_path2 = W.b.aspect2d.attachNewNode(b2)
-    frame_node_path = W.b.aspect2d.attachNewNode(F)
-    frame_node_path2 = W.b.aspect2d.attachNewNode(F2)
-    
-    button_node_path.set_scale(0.15)
-    button_node_path.set_pos(-0.8,0,0.15)
-    
-    button_node_path2.set_scale(0.3)
-    button_node_path2.set_pos(0,0,0.5)
-    
-    frame_node_path.set_scale(1)
-    frame_node_path.set_pos(-0.3,0,-0.3)
-    frame_node_path.set_hpr(0,0,45)
-    
-    frame_node_path2.set_scale(1)
-    frame_node_path2.set_pos(0.3,0,-0.5)
-    frame_node_path2.set_hpr(45,45,0)
-    
-    while True:
-        W.b.taskMgr.step()
+	W = Wrapper()
+
+	
+	while True:
+		W.b.taskMgr.step()
 
 if __name__ == "__main__":
-    main()
+	main()
